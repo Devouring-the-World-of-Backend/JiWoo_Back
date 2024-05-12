3a3d59366df11e0ad6681130a138ef19ca116110
JiwooHong1020 <165543931+JiwooHong1020@users.noreply.github.com>
2024-05-12 13:25:44 +0900

    bookrouter.py

--- routers/bookrouter.py
+++ routers/bookrouter.py
@@ -1,15 +1,14 @@
 from fastapi import APIRouter, HTTPException, Depends
-from db.database import Session
+from db.database import SessionLocal, get_db
 from db.models import Book
+from sqlalchemy.orm import Session
 from schemas import book
 import book_crud
+import contextlib
 
 br = APIRouter(
     prefix="/library"
 )
-async def get_db():
-    async with Session as session:
-        yield session
 
 @br.get("/")
 def root():
@@ -17,38 +16,37 @@ def root():
 
 #Create
 @br.post("/books", status_code=201, response_model=book.Book)
-async def create_book(book: book.BookCreate):
-    db = Session()
-    data = await book_crud.create_book(db=db, book=book)
-    db.close()
+def create_book(book: book.BookCreate, db: Session = Depends(get_db)):
+    print(type(db))
+    data = book_crud.create_book(db=db, book=book)
     return data
 # Read
 @br.get("/books")
-async def get_books():
+def get_books():
     db = Session()
-    datas = await book_crud.get_books(db=db)
+    datas = book_crud.get_books(db=db)
     db.close()
     return datas
 
 @br.get("/books/search", status_code = 201,response_model = book.BookSearch)
-async def search_book():
+def search_book():
     db = Session()
-    data = await book_crud.search_book(db=db, book = book)
+    data = book_crud.search_book(db=db, book = book)
     db.close()
     return data
 # Update
 @br.put("/books/{book_id}", response_model = book.BookUpdate)
-async def update_book(id: int, book = book.Book):
+def update_book(id: int, book = book.Book):
     db = Session()
-    data = await book_crud.update_book(db=db, book = book, book_id = id)
+    data = book_crud.update_book(db=db, book = book, book_id = id)
     db.close()
     print("Update Succeed")
     return data
 # Delete
 @br.delete("/books/{book_id}")
-async def delete_book(id: int):
+def delete_book(id: int):
     db = Session()
-    data = await book_crud.delete_book(db = db, book_id = id)
+    data = book_crud.delete_book(db = db, book_id = id)
     db.close()
     print("Deletion Succeed")
     return data
