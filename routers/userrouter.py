6346d4f7113cde80723419326d0afbfc6b28d32d
JiwooHong1020 <165543931+JiwooHong1020@users.noreply.github.com>
2024-05-12 13:26:11 +0900

    Update userrouter.py

--- routers/userrouter.py
+++ routers/userrouter.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException, Depends
-from db.database import Session
+from db.database import SessionLocal
 from db.models import Book, User
 from schemas import book, user
 import user_crud
@@ -10,12 +10,12 @@ ur = APIRouter(
 
 # Create
 @ur.post("/users")
-async def create_user(user: user.UserCreate):
-    db = Session()
+def create_user(user: user.UserCreate):
+    db = SessionLocal()
     user_data = user_crud.create_user(db=db, user=user)
     db.close()
     return user_data
 # Active State
 @ur.post("/users/{user_id}")
-async def active_user(user: user.UserBase):
-    db = Session()
+def active_user(user: user.UserBase):
+    db = SessionLocal()
